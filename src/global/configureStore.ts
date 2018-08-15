0c5c4569ff73500efeeac010e8b2ee891fbb073b
riskers <gaoyibobobo@gmail.com>
2018-08-15 23:46:14 +0800

    update

--- src/global/configureStore.ts
+++ src/global/configureStore.ts
@@ -3,7 +3,6 @@ import thunk from "redux-thunk";
 import { homeReducers } from "@/pages/Home/flow/homeReducers";
 import { globalReducers } from "./globalReducers";
 
-/* eslint-disable no-underscore-dangle, no-undef */
 const composeEnhancers = (window as any) && (window as any).REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 const reducer = combineReducers({
   global: globalReducers,
